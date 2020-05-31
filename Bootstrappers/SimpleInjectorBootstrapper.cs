271c95b50273dd837c2b38a647d63de53912109f
Mark <markpardoe@gmail.com>
2020-05-31 23:06:12 +0100

    Added SimpleInjector bootstrapper & tests.

---
+++ Bootstrappers/SimpleInjectorBootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using SimpleInjector;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Bootstrappers
+{
+    public class SimpleInjectorBootstrapper<TRootViewModel> : BootstrapperBase where TRootViewModel : class
+    {
+        private Container _container;
+        private object _rootViewModel;
+
+        protected virtual object RootViewModel => this._rootViewModel ?? (this._rootViewModel = this.GetInstance(typeof(TRootViewModel)));
+
+        protected override void ConfigureBootstrapper()
+        {
+            Container container = new Container();
+            DefaultConfigureIoC(container);
+            ConfigureIoC(container);
+
+            container.Verify();
+            _container = container;
+        }
+
+        /// <summary>
+        /// Carries out default configuration of the IoC container. Override if you don't want to do this
+        /// </summary>
+        protected virtual void DefaultConfigureIoC(Container container)
+        {
+            var viewManagerConfig = new ViewManagerConfig()
+            {
+                ViewFactory = this.GetInstance,
+                ViewAssemblies = new List<Assembly>() { this.GetType().Assembly }
+            };
+            var viewManager = new ViewManager(viewManagerConfig);
+            container.RegisterInstance<IViewManager>(viewManager);
+
+            container.RegisterInstance<IWindowManagerConfig>(this);
+            container.RegisterInstance<IWindowManager>(new WindowManager(viewManager, container.GetInstance<IMessageBoxViewModel>, this));
+            container.Register<IEventAggregator, EventAggregator>(Lifestyle.Singleton);
+            container.Register<IMessageBoxViewModel, MessageBoxViewModel>(Lifestyle.Transient); // Not singleton
+        }
+
+        /// <summary>
+        /// Override to add your own types to the IoC container.
+        /// </summary>
+        protected virtual void ConfigureIoC(Container container) { }
+
+        public override object GetInstance(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+
+        protected override void Launch()
+        {
+            base.DisplayRootView(this.RootViewModel);
+        }
+
+        public override void Dispose()
+        {
+            ScreenExtensions.TryDispose(this._rootViewModel);
+            _container?.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Bootstrappers/Tests/SimpleInjectorTests.cs
@@ -0,0 +1,70 @@
+﻿using SimpleInjector;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrappers.Tests
+{
+    public class MySimpleInjectorBootstrapper : SimpleInjectorBootstrapper<TestRootViewModel>, ITestBootstrapper
+    {
+        public List<string> ConfigureLog { get; set; }
+
+        public int DisposeCount { get; private set; }
+
+        public MySimpleInjectorBootstrapper()
+        {
+            this.ConfigureLog = new List<string>();
+        }
+
+        protected override void Configure()
+        {
+            base.Configure();
+            this.ConfigureLog.Add("Configure");
+        }
+
+        protected override void DefaultConfigureIoC(Container container)
+        {
+            base.DefaultConfigureIoC(container);
+            this.ConfigureLog.Add("DefaultConfigureIoC");
+        }
+
+        protected override void ConfigureIoC(Container container)
+        {
+            base.ConfigureIoC(container);
+            this.ConfigureLog.Add("ConfigureIoC");
+        }
+
+        public new object GetInstance(Type type)
+        {
+            return base.GetInstance(type);
+        }
+
+        public new void ConfigureBootstrapper()
+        {
+            base.ConfigureBootstrapper();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            this.DisposeCount++;
+        }
+    }
+
+    [TestFixture(Category = "SimpleInjector")]
+    public class SimpleInjectorTests : BootstrapperTests<MySimpleInjectorBootstrapper>
+    {
+        public SimpleInjectorTests()
+        {
+            this.Autobinds = true;
+        }
+
+        public override MySimpleInjectorBootstrapper CreateBootstrapper()
+        {
+            return new MySimpleInjectorBootstrapper();
+        }
+    }
+}
